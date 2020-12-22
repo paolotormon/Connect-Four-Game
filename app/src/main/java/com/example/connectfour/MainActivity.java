b19323b5f43abfeb6c022e404096cc03e6a57c8d
Paolo Tormon <mdawntormon@gmail.com>
2020-12-22 12:06:04 +0800

    Fix Gradle Build

--- app/src/main/java/com/example/connectfour/MainActivity.java
+++ app/src/main/java/com/example/connectfour/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     androidx.gridlayout.widget.GridLayout gridLayoutBoard;
     int [] gameState= new int[42];
     int[][] winningPositions= {
-            //horizontal
+            //horizontal --24
             {0,1,2,3},{1,2,3,4},{2,3,4,5},{3,4,5,6},
             {7,8,9,10},{8,9,10,11},{9,10,11,12},{10,11,12,13},
             {14,15,16,17},{15,16,17,18},{16,17,18,19},{17,18,19,20},
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
             {28,29,30,31},{29,30,31,32},{30,31,32,33},{31,32,33,34},
             {35,36,37,38},{36,37,38,39},{37,38,39,40},{38,39,40,41},
 
-            //vertical
+            //vertical --21
             {0,7,14,21},{7,14,21,28},{14,21,28,35},
             {1,8,15,22},{8,15,22,29},{15,22,29,36},
             {2,9,16,23},{9,16,23,30},{16,23,30,37},
@@ -40,15 +40,17 @@ public class MainActivity extends AppCompatActivity {
             {5,12,19,26},{12,19,26,33},{19,26,33,40},
             {6,13,20,27},{13,20,27,34},{20,27,34,41},
 
-            //diagonal (top left to bottom right)
+            //diagonal (top left to bottom right) --12
             {0,8,16,24},{1,9,17,25},{2,10,18,26},{3,11,19,27},
             {7,15,23,31},{8,16,24,32},{9,17,25,31},{10,18,26,32},
             {14,22,30,38},{15,23,31,39},{16,24,32,40},{17,25,33,41},
 
-            //diagonal(top right to bottom left)
+            //diagonal(top right to bottom left) --12
             {3,9,15,21},{4,10,16,22},{5,11,17,23},{6,12,18,24},
             {10,16,22,28},{11,17,23,29},{12,18,24,30},{13,19,25,31},
             {17,23,29,35},{18,24,30,36},{19,25,31,37},{20,26,32,38},
+
+            //-24+21+12+12 = 69
     };
 
     public void dropToken(View view){
